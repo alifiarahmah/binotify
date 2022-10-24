43e2dc6b7838505b669a65d1d058780bc35fdf35
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-25 05:15:25 +0700

    feat: add design system

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,23 @@
-This is a homepage
+<h1 class="logo">binotify</h1>
+
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+<p>Paragraph</p>
+
+<div>
+    <p>
+        What happens if there was a
+        <a href="#">link</a> in this paragraph?
+    </p>
+    <a href="#" target="_blank"><button class="button-solid">button link</button></a>
+</div>
+
+<div>
+    <button class="button-solid">solid</button>
+    <button class="button-outline">outline</button>
+    <button class="button"></button>unstyled</button>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,10 +2,21 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= $data['title'] ?></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <style>
+        <?php include 'public/css/animation.css'; ?>
+        <?php include 'public/css/colors.css'; ?>
+        <?php include 'public/css/globals.css'; ?>
+        <?php include 'public/css/typography.css'; ?>
+        
+        <?php include 'public/css/components/button.css'; ?>
+    </style>
+
+    <title><?= $data['title'] ?></title>
 </head>
 
 <body>
