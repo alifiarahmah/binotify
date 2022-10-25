4e099e3d2a4e2871db460a7bc04face44800e8f4
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-25 11:22:13 +0700

    feat(home): add song list for mobile

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,10 +4,32 @@ class Home extends Controller
 {
     public function index()
     {
-        $data['title'] = 'Welcome!';
+        $data['title'] = 'Listen to all songs';
         $this->view('templates/header', $data);
-        $this->view('templates/navbar');
-        $this->view('home/index');
+        $this->view('templates/navbar', [
+            'role' => 'user'
+        ]);
+        $this->view('home/index', [
+            'songs' => [[
+                'id' => 1,
+                'title' => 'Aku Wanita',
+                'artist' => 'Penyanyi Wanita',
+                'year' => '2092',
+                'genre' => 'Pop'
+            ], [
+                'id' => 2,
+                'title' => 'Aku Pria',
+                'artist' => 'Penyanyi Pria',
+                'year' => '2042',
+                'genre' => 'Pop'
+            ], [
+                'id' => 3,
+                'title' => 'Aku Tidak Tahu',
+                'artist' => 'Penyanyi Tanpa Jenis Kelamin',
+                'year' => '2022',
+                'genre' => 'Pop'
+            ]]
+        ]);
         $this->view('templates/footer');
     }
 }
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,33 +1,62 @@
-<h1>Heading 1</h1>
-<h2>Heading 2</h2>
-<h3>Heading 3</h3>
-<h4>Heading 4</h4>
-<h5>Heading 5</h5>
-<h6>Heading 6</h6>
-<p>Paragraph</p>
+<h1>
+    Listen to curated songs
+</h1>
 
-<div>
-    <p>
-        What happens if there was a
-        <a href="#">link</a> in this paragraph?
-    </p>
-    <a href="#" target="_blank"><button class="button-solid">button link</button></a>
-</div>
+<!-- single song item (mobile) -->
+<?php foreach ($data['songs'] as $song) { ?>
+    <a href="#" class="song-item-container-mobile">
+        <div class="song-item">
+            <div class="song-item-data">
+                <p>1</p>
+                <div class="song-picture">
+                    <image src="http://source.unsplash.com/random/42x42">
+                </div>
+                <div>
+                    <h3><?= $song['title']; ?></h3>
+                    <p><?= $song['artist']; ?> · <?= $song['year']; ?> · <?= $song['genre']; ?></p>
+                </div>
+            </div>
+            <div>
+                <svg width="20" height="6" viewBox="0 0 20 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.99999 0.666626C1.71666 0.666626 0.666656 1.71663 0.666656 2.99996C0.666656 4.28329 1.71666 5.33329 2.99999 5.33329C4.28332 5.33329 5.33332 4.28329 5.33332 2.99996C5.33332 1.71663 4.28332 0.666626 2.99999 0.666626ZM17 0.666626C15.7167 0.666626 14.6667 1.71663 14.6667 2.99996C14.6667 4.28329 15.7167 5.33329 17 5.33329C18.2833 5.33329 19.3333 4.28329 19.3333 2.99996C19.3333 1.71663 18.2833 0.666626 17 0.666626ZM9.99999 0.666626C8.71666 0.666626 7.66666 1.71663 7.66666 2.99996C7.66666 4.28329 8.71666 5.33329 9.99999 5.33329C11.2833 5.33329 12.3333 4.28329 12.3333 2.99996C12.3333 1.71663 11.2833 0.666626 9.99999 0.666626Z" fill="white" />
+                </svg>
+            </div>
+        </div>
+    </a>
+<?php } ?>
 
-<div>
-    <button class="button-solid">solid</button>
-    <button class="button-outline">outline</button>
-    <button class="button"></button>unstyled</button>
-</div>
-
-<div>
-    <input placeholder="unstyled" />
-    <input class="input-solid" placeholder="solid" />
-    <input class="input-outline" placeholder="outline" />
-    <div class="input-solid">
-        <input />
-        <svg width="25" height="17" viewBox="0 0 25 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12.5 0.0625C6.875 0.0625 2.07125 3.56125 0.125 8.5C2.07125 13.4388 6.875 16.9375 12.5 16.9375C18.125 16.9375 22.9287 13.4388 24.875 8.5C22.9287 3.56125 18.125 0.0625 12.5 0.0625ZM12.5 14.125C9.395 14.125 6.875 11.605 6.875 8.5C6.875 5.395 9.395 2.875 12.5 2.875C15.605 2.875 18.125 5.395 18.125 8.5C18.125 11.605 15.605 14.125 12.5 14.125ZM12.5 5.125C10.6325 5.125 9.125 6.6325 9.125 8.5C9.125 10.3675 10.6325 11.875 12.5 11.875C14.3675 11.875 15.875 10.3675 15.875 8.5C15.875 6.6325 14.3675 5.125 12.5 5.125Z" fill="#2A2B30" />
-        </svg>
-    </div>
+<!-- single song item (desktop) -->
+<div class="song-item-container-desktop">
+    <table>
+        <tr>
+            <th></th>
+            <th></th>
+            <th>song title</th>
+            <th>artist</th>
+            <th>year</th>
+            <th>genre</th>
+            <th></th>
+        </tr>
+        <?php foreach ($data['songs'] as $song) { ?>
+            <tr>
+                <td>1</td>
+                <td>
+                    <div class="song-picture">
+                        <image src="http://source.unsplash.com/random/42x42">
+                    </div>
+                </td>
+                <td><?= $song['title']; ?></td>
+                <td><?= $song['artist']; ?></td>
+                <td><?= $song['year']; ?></td>
+                <td><?= $song['genre']; ?></td>
+                <td>
+                    <div>
+                        <svg width="20" height="6" viewBox="0 0 20 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.99999 0.666626C1.71666 0.666626 0.666656 1.71663 0.666656 2.99996C0.666656 4.28329 1.71666 5.33329 2.99999 5.33329C4.28332 5.33329 5.33332 4.28329 5.33332 2.99996C5.33332 1.71663 4.28332 0.666626 2.99999 0.666626ZM17 0.666626C15.7167 0.666626 14.6667 1.71663 14.6667 2.99996C14.6667 4.28329 15.7167 5.33329 17 5.33329C18.2833 5.33329 19.3333 4.28329 19.3333 2.99996C19.3333 1.71663 18.2833 0.666626 17 0.666626ZM9.99999 0.666626C8.71666 0.666626 7.66666 1.71663 7.66666 2.99996C7.66666 4.28329 8.71666 5.33329 9.99999 5.33329C11.2833 5.33329 12.3333 4.28329 12.3333 2.99996C12.3333 1.71663 11.2833 0.666626 9.99999 0.666626Z" fill="white" />
+                        </svg>
+                    </div>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 </div>
