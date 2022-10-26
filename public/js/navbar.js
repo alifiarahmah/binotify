e3c12dc58de9e2800ad592cca26d646d8f85ef66
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-26 11:30:30 +0700

    fix(navbar): fix search alignment in mobile

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,6 @@
 function showSearch() {
 	var searchIcon = document.getElementById("search-icon");
-	var search = document.getElementById("search-bar");
+	var search = document.getElementById("search-bar-container");
 	if (search.style.display === "none") {
 			search.style.display = "block";
 	} else {
