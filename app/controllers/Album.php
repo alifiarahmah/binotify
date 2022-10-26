1fe73a539dc8467d25f6a8783a57e72017e606b5
dikyrest <16520040@std.stei.itb.ac.id>
2022-10-27 06:33:44 +0700

    refactor(album): integrate album details

--- app/controllers/Album.php
+++ app/controllers/Album.php
@@ -2,22 +2,50 @@
 
 class Album extends Controller
 {
-    public function index($id = 0)
-    {
-        $data['title'] = 'Album';
-        $this->view('templates/layout', [
-            'id' => $id
-        ]);
-    }
+	public function index($current_page = 1)
+	{
+		$data['title'] = 'All Albums';
+		$this->view('templates/layout', [
+			'detail' => false,
+			'current_page' => $current_page
+		]);
+	}
 
-    public function fetch($data = [])
-    {
-        $data['content'] = 'album/index';
-        $album = $this->model('Album_model')->getAlbumById($data['id']);
-        $songs = $this->model('Song_model')->getSongByAlbumId($data['id']);
-        $this->view($data['content'], [
-            'album' => $album,
-            'songs' => $songs
-        ]);
-    }
+	public function detail($id = 0)
+	{
+		$data['title'] = 'Album';
+		$this->view('templates/layout', [
+			'detail' => true,
+			'id' => $id,
+		]);
+	}
+
+	public function fetch($data = [])
+	{
+		if ($data['detail']) {
+			$data['content'] = 'album/detail';
+			$album = $this->model('Album_model')->getAlbumById($data['id']);
+			$songs = $this->model('Song_model')->getSongByAlbumId($data['id']);
+			$this->view($data['content'], [
+				'album' => $album,
+				'songs' => $songs
+			]);
+		} else {
+			$current_page = $data['current_page'];
+
+			// pagination
+			$item_per_page = 5;
+			$item_count = $this->model('Album_model')->getAlbumNums();
+			$total_page = ceil($item_count / $item_per_page);
+			$first_item = ($item_per_page * $current_page) - $item_per_page;
+			$albums = $this->model('Album_model')->getNAlbums($first_item, $item_per_page);
+			
+			$data['content'] = 'album/index';
+			$this->view($data['content'], [
+				'albums' => $albums,
+				'current_page' => $current_page,
+				'total_page' => $total_page
+			]);
+		}
+	}
 }
--- app/views/album/index.php
+++ app/views/album/index.php
@@ -1,39 +1,31 @@
-<section id="album">
-    <?php if ($data['album']) { ?>
-        <div class="album-details">
-            <div class="album-details" id="album-picture">
-                <img src="<?= $data['album']['image_path'] ?>" width="150" height="150">
-            </div>
-            <div class="album-info">
-                <h1><?= $data['album']['album_title'] ?></h1>
-                <p><?= $data['album']['album_artist'] ?> - <?= $data['album']['tanggal_terbit'] ?> - <?= $data['album']['total_duration'] ?> s total</p>
-            </div>
-        </div>
-        <div class="song-item-container">
-            <div class="header-row"></div>
-            <div class="header-row"></div>
-            <div class="header-row header-title">song title</div>
-            <div class="header-row header-artist">artist</div>
-            <div class="header-row header-date">date</div>
-            <div class="header-row header-genre">genre</div>
-            <?php if (count($data['songs']) > 0) { ?>
-                <?php foreach ($data['songs'] as $i => $song) { ?>
-                    <a class="content-row" href="<?= BASE_URL ?>/song/<?= $song['song_id'] ?>">
-                        <div class="song-number"><?= $i + 1 ?></div>
-                        <div class="song-picture">
-                            <image src="<?= $song['image_path'] ?? "https://binotify.blob.core.windows.net/photo/placeholder.jpg" ?>" width="42px" height="42px">
-                        </div>
-                        <div class="song-title"><?= $song['song_title']; ?></div>
-                        <div class="song-artist"><?= $song['song_artist']; ?></div>
-                        <div class="song-date"><?= $song['release_date']; ?></div>
-                        <div class="song-genre"><?= $song['genre']; ?></div>
-                    </a>
-                <?php } ?>
-            <?php } else { ?>
-                <p>no songs found</p>
-            <?php } ?>
-        </div>
-    <?php } else { ?>
-        <h1>404 not found</h1>
-    <?php } ?>
+<section id="albums">
+	<h1>All Albums</h1>
+
+	<?php if (count($data['albums']) > 0) { ?>
+		<div class="song-item-container">
+			<div class="header-row"></div>
+			<div class="header-row"></div>
+			<div class="header-row header-title">album title</div>
+			<div class="header-row header-artist">artist</div>
+			<div class="header-row header-date">date</div>
+			<div class="header-row header-genre">genre</div>
+			<?php foreach ($data['albums'] as $i => $album) { ?>
+				<a class="content-row" href="<?= BASE_URL ?>/album/<?= $album['album_id'] ?>">
+					<div class="album-number"><?= $i + 1 ?></div>
+					<div class="album-picture">
+						<image src="<?= $album['image_path'] ?? "https://binotify.blob.core.windows.net/photo/placeholder.jpg" ?>" width="42px" height="42px">
+					</div>
+					<div class="album-title"><?= $album['album_title']; ?></div>
+					<div class="album-artist"><?= $album['album_artist']; ?></div>
+					<div class="album-date"><?= $album['tanggal_terbit']; ?></div>
+					<div class="album-genre"><?= $album['genre']; ?></div>
+				</a>
+			<?php } ?>
+		<?php } else { ?>
+			<p>No albums found</p>
+		</div>
+	<?php } ?>
+
 </section>
+<?php $base_url = BASE_URL . '/albums' ?>
+<?php require_once __DIR__ . '/../templates/pagination.php' ?>
