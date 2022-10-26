538d44aed93857b34e03eb70e57ec9a5281b4ee6
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-26 13:21:10 +0700

    config: add default configuration + add .gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'Thathenk96');
+define('DB_PASS', '');
 define('DB_NAME', 'binotify');
 
 define('BASE_URL', '/tugas-besar-1');
