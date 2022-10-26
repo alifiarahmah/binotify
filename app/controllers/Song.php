654fffec78292edac27287c0a3af013d28bac300
dikyrest <16520040@std.stei.itb.ac.id>
2022-10-26 14:36:36 +0700

    feat(template): add parameter

--- app/controllers/Song.php
+++ app/controllers/Song.php
@@ -2,11 +2,12 @@
 
 class Song extends Controller
 {
-    public function index()
+    public function index($id = 1)
     {
         $data['title'] = 'Song';
         $this->view('templates/header', $data);
         $this->view('templates/navbar');
+<<<<<<< Updated upstream
         $this->view('templates/layout');
         $this->view('templates/footer');
     }
@@ -16,4 +17,25 @@ class Song extends Controller
         $data['content'] = 'song/index';
         $this->view($data['content']);
     }
+=======
+<<<<<<< Updated upstream
+        $this->view('templates/sidebar');
+        $this->view('songdetails/index');
+        $this->view('templates/footer');
+    }
+=======
+        $this->view('templates/layout', [
+            'id' => $id,
+        ]);
+        $this->view('templates/footer');
+    }
+
+    public function fetch($data = [])
+    {
+        $data['content'] = 'song/index';
+        $data['song'] = $this->model('Song_model')->getSongById($data['id']);
+        $this->view($data['content'], $data['song']);
+    }
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
