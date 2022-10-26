3b9f8b949654d9c1589aca873af0463ea3d7e15e
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-26 11:55:07 +0700

    refactor(album): rename Albums controller to Album

--- app/controllers/Album.php
+++ app/controllers/Album.php
@@ -1,6 +1,6 @@
 <?php
 
-class Albums extends Controller
+class Album extends Controller
 {
 	public function index($current_page = 1)
 	{
@@ -9,7 +9,7 @@ class Albums extends Controller
 		$item_count = $this->model('Album_model')->getAlbumNums();
 		$total_page = ceil($item_count / $item_per_page);
 		$first_item = ($item_per_page * $current_page) - $item_per_page;
-		$data = $this->model('Album_model')->getAlbums($first_item, $item_per_page);
+		$data = $this->model('Album_model')->getNAlbums($first_item, $item_per_page);
 
 
 		$this->view('templates/header', [
@@ -20,6 +20,22 @@ class Albums extends Controller
 			'albums' => $data,
 			'total_page' => $total_page,
 			'current_page' => $current_page
+		]);
+		$this->view('templates/footer');
+	}
+
+	public function search($search_query = "")
+	{
+		// pagination
+
+
+
+		$this->view('templates/header', [
+			'title' => 'Search Results'
+		]);
+		$this->view('templates/navbar');
+		$this->view('albums/search', [
+			'q' => $search_query,
 		]);
 		$this->view('templates/footer');
 	}
