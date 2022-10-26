b63c230ec39ea5ce2c39798c55dd8ab224aa0051
dikyrest <16520040@std.stei.itb.ac.id>
2022-10-26 16:20:10 +0700

    fix(songs): fix song details display

--- app/views/song/index.php
+++ app/views/song/index.php
@@ -1,7 +1,15 @@
 <section id="song">
-    <h1>Song Details</h1>
-
-    <div class="song-details-container">
-        Ini adalah halaman song details
+    <div class="song-details">
+        <div class="song-details" id="album-picture">
+            <img src="<?= $data[0]['image_path'] ?>" width="150" height="150">
+        </div>
+        <div class="song-info">
+            <h1><?= $data[0]['song_title'] ?></h1>
+            <p><?= $data[0]['song_artist'] ?> - <?= $data[0]['release_date'] ?> - <?= $data[0]['duration'] ?> s</p>
+        </div>
+    </div>
+    <div class="audio-player">
+        <audio controls>
+            <source src="<?= $data[0]['audio_path']; ?>" type="audio/mpeg">
     </div>
 </section>
