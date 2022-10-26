19f6e2a90afb34382f208d8ff8dd26d2aaa41e9e
dikyrest <16520040@std.stei.itb.ac.id>
2022-10-27 03:02:24 +0700

    feat(album): add album details page

--- app/controllers/Album.php
+++ app/controllers/Album.php
@@ -2,30 +2,25 @@
 
 class Album extends Controller
 {
-	public function index($current_page = 1)
-	{
-		$data['title'] = 'All Albums';
-		$this->view('templates/layout', [
-			'current_page' => $current_page
-		]);
-	}
+    public function index($id = 1)
+    {
+        $data['title'] = 'Album';
+        $this->view('templates/header', $data);
+        $this->view('templates/navbar');
+        $this->view('templates/layout', [
+            'id' => $id
+        ]);
+        $this->view('templates/footer');
+    }
 
-	public function fetch($data = [])
-	{
-		$current_page = $data['current_page'];
-
-		// pagination
-		$item_per_page = 5;
-		$item_count = $this->model('Album_model')->getAlbumNums();
-		$total_page = ceil($item_count / $item_per_page);
-		$first_item = ($item_per_page * $current_page) - $item_per_page;
-		$albums = $this->model('Album_model')->getNAlbums($first_item, $item_per_page);
-		$data['content'] = 'albums/index';
-
-		$this->view($data['content'], [
-			'albums' => $albums,
-			'total_page' => $total_page,
-			'current_page' => $current_page
-		]);
-	}
+    public function fetch($data = [])
+    {
+        $data['content'] = 'album/index';
+        $album = $this->model('Album_model')->getAlbumById($data['id']);
+        $songs = $this->model('Song_model')->getSongByAlbumId($data['id']);
+        $this->view($data['content'], [
+            'album' => $album,
+            'songs' => $songs
+        ]);
+    }
 }
