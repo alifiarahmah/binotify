5b4e2cb69057252ac305fc2a2d7363df96b7424e
dikyrest <16520040@std.stei.itb.ac.id>
2022-10-28 09:45:24 +0700

    fix: admin session

--- app/views/album/detail.php
+++ app/views/album/detail.php
@@ -7,22 +7,6 @@
             <div class="album-info">
                 <h1><?= $data['album']['album_title'] ?></h1>
                 <p><?= $data['album']['album_artist'] ?> - <?= $data['album']['tanggal_terbit'] ?> - <?= $data['album']['total_duration'] ?> s total</p>
-<<<<<<< HEAD
-                <!-- admin: edit and delete album -->
-                <?php if ($_SESSION['isAdmin']) { ?>
-                    <div class="album-actions">
-                        <a class="edit-button" href="<?= BASE_URL ?>/album/edit/<?= $data['album']['album_id'] ?>">
-                            <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0 29.5001V37.0001H7.5L29.62 14.8801L22.12 7.38012L0 29.5001ZM35.42 9.08012C36.2 8.30012 36.2 7.04012 35.42 6.26012L30.74 1.58012C29.96 0.800117 28.7 0.800117 27.92 1.58012L24.26 5.24012L31.76 12.7401L35.42 9.08012Z" fill="#FFDD3C"/>
-                            </svg>
-                        </a>
-                        <a class="delete-button" href="<?= BASE_URL ?>/album/delete/<?= $data['album']['album_id'] ?>">
-                            <svg width="28" height="36" viewBox="0 0 28 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 32C2 34.2 3.8 36 6 36H22C24.2 36 26 34.2 26 32V8H2V32ZM28 2H21L19 0H9L7 2H0V6H28V2Z" fill="#FF646C"/>
-                            </svg>
-                        </a>
-                    </div>
-=======
                 <?php
                 if (isset ($_SESSION['isAdmin']) && $_SESSION['isAdmin'] == true) {
                 ?>
@@ -38,7 +22,6 @@
                         </svg>
                     </a>
                 </div>
->>>>>>> 3de73f721427334724cff3d3311fe2e3dbb934c6
                 <?php } ?>
             </div>
         </div>
