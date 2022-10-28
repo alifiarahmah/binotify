99c2c00daf1c5e837d1b2716b9e7249a6fec24c1
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2022-10-28 12:52:27 +0700

    feat(api): add search api

--- app/controllers/api.php
+++ app/controllers/api.php
@@ -29,4 +29,25 @@ class Api extends Controller
 			echo json_encode($albums);
 		}
 	}
+
+	public function search($keyword = "", $sort = "", $filter = "all", $current_page = 1)
+	{
+		$query = $this->model('Song_model')->searchSong($keyword, $sort, $filter, $current_page);
+
+		// pagination
+		$item_per_page = 5;
+		$item_count = $query['count'];
+		$total_page = ceil($item_count / $item_per_page);
+		$songs = $this->model('Song_model')->getNSearchSongs($keyword, $sort, $filter, $current_page);
+
+		echo json_encode([
+			'search' => $keyword,
+			'songs' => $songs,
+			'item_count' => $item_count,
+			'current_page' => $current_page,
+			'total_page' => $total_page,
+			'sort' => $sort,
+			'filter' => $filter
+		]);
+	}
 }
